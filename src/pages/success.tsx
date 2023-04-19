0f97451d5836a77b116097fb9c960ce4748729e0
Bruno Patez <patezbr@gmail.com>
2023-04-19 19:06:24 -0300

    update: redirect ssr

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -33,22 +33,40 @@ export default function Success({ customerName, product }: SuccessProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
-  const sessionId = String(query.session_id)
-
-  const session = await stripe.checkout.sessions.retrieve(sessionId, {
-    expand: ['line_items', 'line_items.data.price.product'],
-  })
-
-  const customerName = session.customer_details?.name
-  const product = session.line_items?.data[0].price?.product as Stripe.Product
-
-  return {
-    props: {
-      customerName,
-      product: {
-        name: product.name,
-        imageUrl: product.images[0],
+  if (!query.session_id) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
       },
-    },
+    }
+  }
+
+  try {
+    const sessionId = String(query.session_id)
+
+    const session = await stripe.checkout.sessions.retrieve(sessionId, {
+      expand: ['line_items', 'line_items.data.price.product'],
+    })
+
+    const customerName = session.customer_details?.name
+    const product = session.line_items?.data[0].price?.product as Stripe.Product
+
+    return {
+      props: {
+        customerName,
+        product: {
+          name: product.name,
+          imageUrl: product.images[0],
+        },
+      },
+    }
+  } catch (error) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
   }
 }
