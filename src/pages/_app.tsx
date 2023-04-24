1e0848aaf676e97d7be2a96526e02f3075e6e09c
Bruno Patez <patezbr@gmail.com>
2023-04-24 18:02:47 -0300

    update: checkout page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
 import type { AppProps } from 'next/app'
 import Image from 'next/image'
-import { globalStyles } from '@/styles/global'
-import { ButtonContainer, Container, Header } from '@/styles/pages/app'
-import logoImg from '../assets/logo.svg'
 import Link from 'next/link'
 import { ShoppingCart } from 'phosphor-react'
+import logoImg from '../assets/logo.svg'
+import { globalStyles } from '@/styles/global'
+import { ButtonContainer, Container, Header } from '@/styles/pages/app'
 
 globalStyles()
 
@@ -15,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <Link href="/">
           <Image src={logoImg} alt="" width={130} height={52} />
         </Link>
+
         <Link href="/checkout">
           <ButtonContainer>
             <ShoppingCart size={32} color="#8D8D99" weight="bold" />
---
+++ src/pages/checkout/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Checkout() {
+  return (
+    <>
+      <Head>
+        <title>Carrinho - Ignite Shop</title>
+        <meta name="robots" content="noindex" />
+      </Head>
+    </>
+  )
+}
